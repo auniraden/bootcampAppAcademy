667b8769e3d23793d8e79bfebb8b641696528056
AuniShah <radennn.auni@gmail.com>
2023-09-11 16:26:08 +0800

    completed the 2D arrays sums and pairs! :)

---
+++ 1.9 Advanced-arrays/2Dsum.js
@@ -0,0 +1,26 @@
+let twoDimensionalSum = function(arr){
+    let sum = 0;
+
+    for(let i = 0; i < arr.length; i++){
+        let subArray = arr[i];
+        for(let j = 0; j < subArray.length; j = j + 1){
+            sum = sum + subArray[j];
+        }
+    }
+    return sum;
+}
+
+
+
+let arr1 = [
+    [1, 3],
+    [-4, 7, 10],
+    [2]
+];
+console.log(twoDimensionalSum(arr1)); // 19
+
+let arr2 = [
+    [],
+    [3, 1, 2],
+];
+console.log(twoDimensionalSum(arr2)); // 6
