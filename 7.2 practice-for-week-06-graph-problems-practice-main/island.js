66c693d7785a083258b362806ac817c07109bd41
AuniShah <radennn.auni@gmail.com>
2023-10-26 10:22:23 +0800

    about island

--- 7.2 practice-for-week-06-graph-problems-practice-main/island.js
+++ 7.2 practice-for-week-06-graph-problems-practice-main/island.js
@@ -1,43 +1,54 @@
 function getNeighbors(row, col, graph) {
+  const neighbors = [];
 
   // Check top
+  if (row > 0) {
+    neighbors.push([row - 1, col]);
+  }
 
   // Check bottom
+  if (row < graph.length - 1) {
+    neighbors.push([row + 1, col]);
+  }
 
   // Check left
+  if (col > 0) {
+    neighbors.push([row, col - 1]);
+  }
 
   // Check right
+  if (col < graph[0].length - 1) {
+    neighbors.push([row, col + 1]);
+  }
 
-  // Return neighbors
-
-  // Your code here
+  return neighbors;
 }
+
 
 
 function islandSize(row, col, graph) {
+  const visited = new Set();
+  const stack = [[row, col]];
+  const startNodeStr = `${row}-${col}`;
+  visited.add(startNodeStr);
+  let size = 0;
 
-  // Create a visited set to store visited nodes
+  while (stack.length > 0) {
+    const [currentRow, currentCol] = stack.pop();
+    size++;
 
-  // Create a stack, put the starting node in the stack
+    const neighbors = getNeighbors(currentRow, currentCol, graph);
+    for (const [neighborRow, neighborCol] of neighbors) {
+      const neighborNodeStr = `${neighborRow}-${neighborCol}`;
+      if (!visited.has(neighborNodeStr) && graph[neighborRow][neighborCol] === 1) {
+        stack.push([neighborRow, neighborCol]);
+        visited.add(neighborNodeStr);
+      }
+    }
+  }
 
-  // Put the stringified starting node in visited
-
-  // Initialize size to 0
-
-  // While the stack is not empty,
-
-    // Pop the first node
-
-    // DO THE THING (increment size by 1)
-
-    // Then push all the UNVISITED neighbors on top of the stack
-    // and mark them as visited
-    // HINT: This is what your helper function `getNeighbors` is for
-    // HINT: Remember, you're storing your visited nodes as strings!
-
-  // return size
-
-  // Your code here
+  return size;
 }
+
 
 module.exports = [getNeighbors, islandSize];
